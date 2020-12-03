6bce4a8dbeae5fd2287d03de5af72431099c4f96
Hoslack <hoslackochieng@gmail.com>
2020-12-03 09:23:57 +0300

    Add meeting rooms question

---
+++ src/challenges/meetRooms.ts
@@ -0,0 +1,14 @@
+// Given an array of meeting time intervals consisting of start and end times, [[s1, e1], [s2, e2], ....]
+// (si<ei), determine if a person could attend all meetings.
+
+// E.g
+// Input: [[0, 30], [5, 10], [15, 20]]
+// Output: false
+
+const meetingRooms = (intervals: Array<number>[]): boolean => {
+    return true
+}
+
+meetingRooms([[1,2]])
+
+export default meetingRooms
