0bf46cf110d7a414aa8144aeeb5371e3062e4394
Hoslack <hoslackochieng@gmail.com>
2020-12-04 08:24:51 +0300

    Add meetings room II question

---
+++ src/challenges/meetingRoomsII.ts
@@ -0,0 +1,17 @@
+// Given an array of meeting time intervals consisting of start and end times, [[s1, e1], [s2, e2], ....]
+// (si<ei), find the number of conference rooms required.
+
+// E.g
+// Input: [[0, 30], [5, 10], [15, 20]]
+// Output: 2
+
+
+// Input: [[7, 10], [2, 4]]
+// Output: 1
+
+const meetingRoomsII = (intervals: Array<number>[]): number => {
+
+    return 0
+}
+
+export default meetingRoomsII
