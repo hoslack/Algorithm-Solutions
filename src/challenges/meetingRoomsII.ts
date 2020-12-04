465a5e0140a391b99955a16eecd20cebad1fa5e2
Hoslack <hoslackochieng@gmail.com>
2020-12-04 08:42:23 +0300

    Add meetinmg rooms 2 solution

--- src/challenges/meetingRoomsII.ts
+++ src/challenges/meetingRoomsII.ts
@@ -10,8 +10,19 @@
 // Output: 1
 
 const meetingRoomsII = (intervals: Array<number>[]): number => {
-
-    return 0
+    if(intervals.length<2) return intervals.length
+    intervals.sort((a, b) => a[0] - b[0])
+    let roomTime = [intervals[0][1]]
+    for(let i = 1; i<intervals.length; i++){
+        let [start, end] = [...intervals[i]]
+        let earliest = Math.min(...roomTime)
+        if(start < earliest){
+            roomTime.push(end)
+        }else{
+            roomTime[roomTime.indexOf(earliest)] = end
+        }
+    }
+    return roomTime.length
 }
 
 export default meetingRoomsII
